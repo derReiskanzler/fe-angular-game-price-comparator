88dc0b90b3806d9a613c4c449a3c72899978432d
Nam Anh Nguyen <nang4525@bht-berlin.de>
2023-11-19 21:07:33 +0100

    chore(home-page): add scaffold for home page

---
+++ src/app/features/home/home-shell.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { HomeShellRoutingModule } from './home-shell-routing.module';
+
+@NgModule({
+  imports: [ HomeShellRoutingModule ]
+})
+export class HomeShellModule { }
