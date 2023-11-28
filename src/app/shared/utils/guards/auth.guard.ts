040c07294203d3eb23045f88b7a7d1cac0381d95
Nam Anh Nguyen <nang4525@bht-berlin.de>
2023-11-29 00:41:01 +0100

    chore(auth-guard): add auth guard for profile page

---
+++ src/app/shared/utils/guards/auth.guard.ts
@@ -0,0 +1,15 @@
+
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+
+export const AuthGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+  const token = localStorage.getItem('token') ?? '';
+
+  if (!token) {
+    router.navigate(['/home']);
+    return false;
+  } else {
+    return true;
+  }
+};
