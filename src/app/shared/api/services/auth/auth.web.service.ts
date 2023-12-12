61bdb3545553bbb5174431ddd8e04c0593287f38
Nam Anh Nguyen <nang4525@bht-berlin.de>
2023-12-12 21:30:52 +0100

    chore(auth-api): add auth web service & models

---
+++ src/app/shared/api/services/auth/auth.web.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { LoginApiResponse } from '../../models/login-api-response.model';
+import { RegisterApiResponse } from '../../models/reigster-api-response.model';
+
+@Injectable({ providedIn: 'root' })
+export class AuthWebService {
+  private basePath = 'http://localhost:8080/api';
+  private http = inject(HttpClient);
+
+  public login(email: string, password: string): Observable<LoginApiResponse> {
+    return this.http.post<LoginApiResponse>(`${this.basePath}/v1/auth/login`, { email, password });
+  }
+  
+  public register(email: string, nickname: string, password: string): Observable<RegisterApiResponse> {
+    return this.http.post<RegisterApiResponse>(`${this.basePath}/v1/auth/register`, { email, nickname, password });
+  }
+}
