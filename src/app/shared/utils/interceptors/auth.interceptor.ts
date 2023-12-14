ed81bb51a351ddf5e1dc958973b00f95515db27f
Nam Anh Nguyen <nang4525@bht-berlin.de>
2023-12-14 01:16:59 +0100

    EBE-22: only add auth header for non-auth api calls

--- src/app/shared/utils/interceptors/auth.interceptor.ts
+++ src/app/shared/utils/interceptors/auth.interceptor.ts
@@ -3,7 +3,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 export const AuthInterceptor: HttpInterceptorFn = (request, next) => {
   const token = localStorage.getItem('token');
 
-  if (token) {
+  if (token && !request.url.includes('api/v1/auth')) {
     request = request.clone({
       setHeaders: {
         Authorization: token ? `Bearer ${token}` : '',
