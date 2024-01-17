82b25dd7c72cd42742ff71fad528ad8c94b8860b
Nam Anh Nguyen <nang4525@bht-berlin.de>
2024-01-18 00:34:29 +0100

    chore(fix-selected-game-guard): add check if selected game in state

--- src/app/shared/utils/guards/has-game-selected.guard.ts
+++ src/app/shared/utils/guards/has-game-selected.guard.ts
@@ -1,12 +1,24 @@
 
 import { inject } from '@angular/core';
 import { CanMatchFn, Router } from '@angular/router';
+import { SearchGameFacadeService } from '../../state/facade/search-game.facade.service';
+import { catchError, of, switchMap } from 'rxjs';
 
 export const hasGameSelected: CanMatchFn = (route, state) => {
-  const queryParams = inject(Router).getCurrentNavigation()?.initialUrl.queryParams
-  if (queryParams && !queryParams['name']) {
+  const queryParams = inject(Router).getCurrentNavigation()?.initialUrl.queryParams;
+  if (queryParams && !queryParams['name'] ) {
     return false;
-  } else {
-    return true;
   }
+
+  return inject(SearchGameFacadeService).selectedGame$
+    .pipe(
+      switchMap(selectedGame => {
+        if (!selectedGame.name) {
+          return of(false);
+        } else {
+          return of(true);
+        }
+      }),
+      catchError(() => of(false))
+    );
 };
