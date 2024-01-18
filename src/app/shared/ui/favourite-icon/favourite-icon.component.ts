8933c36664ff36be8668de9416823314fc27514f
Nam Anh Nguyen <nang4525@bht-berlin.de>
2024-01-18 02:38:13 +0100

    chore(favorite-icon): add new component

---
+++ src/app/shared/ui/favourite-icon/favourite-icon.component.ts
@@ -0,0 +1,20 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-favourite-icon',
+  templateUrl: './favourite-icon.component.html',
+  styleUrls: ['./favourite-icon.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FavouriteIconComponent {
+  @Input() isFavourite: boolean|undefined;
+  @Input() size: string = '1.2rem';
+  @Output() favourize = new EventEmitter<void>();
+
+  public mouseEntered = false;
+
+  public onFavourize(event: Event): void {
+    event.stopPropagation();
+    this.favourize.emit();
+  }
+}
---
+++ src/app/shared/ui/favourite-icon/favourite-icon.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FavouriteIconComponent } from './favourite-icon.component';
+
+@NgModule({
+  declarations: [
+    FavouriteIconComponent,
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    FavouriteIconComponent,
+  ],
+})
+export class FavouriteIconModule { }
