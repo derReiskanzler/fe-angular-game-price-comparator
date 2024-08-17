274c5244a3126613ee688c3e7e16a631df0f0de7
Nam Anh Nguyen <nang4525@bht-berlin.de>
2024-08-17 21:05:39 +0200

    chore(log): log api base url env var

--- src/app/shared/services/environment/environment.service.ts
+++ src/app/shared/services/environment/environment.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({ providedIn: 'root' })
 export class EnvironmentService {
   get apiBaseUrl(): string {
+    console.log(window.ENV.API_BASE_URL);
     return window.ENV.API_BASE_URL;
   }
 }
