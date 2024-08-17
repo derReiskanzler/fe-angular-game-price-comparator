72d9e6353c626a86432e682c1e8ad47a5d3fd1de
Nam Anh Nguyen <nang4525@bht-berlin.de>
2024-08-17 19:41:21 +0200

    chore(remove): unused mock const for testing

--- src/app/shared/testing/favourites/favourites-feature-state.mock.ts
+++
@@ -1,9 +0,0 @@
-import { FavouritesFeatureState } from '../../state/reducers/favourites.reducer';
-import { gameMock } from '../search/game.mock';
-
-export const favouritesFeatureStateMock: FavouritesFeatureState = {
-  favourites: [ gameMock ],
-  isLoading: false,
-  error: '',
-};
-
