13178433131100507e83c3b5df56b80978b18096
Nam Anh Nguyen <nang4525@bht-berlin.de>
2024-08-18 23:07:02 +0200

    chore(env-subs): compress env-vars substitution for docker build

--- .ops/entrypoint.sh
+++ .ops/entrypoint.sh
@@ -3,7 +3,7 @@
 # envsubst < /usr/share/nginx/html/assets/env-vars.sample.js > /usr/share/nginx/html/assets/env-vars.js
 
 # Use environment variable substitution to replace environment variables in env-vars.js
-envsubst < /usr/share/nginx/html/assets/env-vars/env-vars.js > /usr/share/nginx/html/assets/env-vars/env-vars.js.tmp
+envsubst < /usr/share/nginx/html/assets/env-vars/env-vars.sample.js > /usr/share/nginx/html/assets/env-vars/env-vars.js.tmp
 mv /usr/share/nginx/html/assets/env-vars/env-vars.js.tmp /usr/share/nginx/html/assets/env-vars/env-vars.js
 
 
